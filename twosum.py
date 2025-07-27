9a4a05fcdfc30f43cae46f345573d3b3ed38f2d2
ayushgarg9 <ayushgarg9707@gmail.com>
2025-07-28 00:57:48 +0530

    updated two sum

--- twosum.py
+++ twosum.py
@@ -3,7 +3,7 @@ target = 13
 n = len(list1)
 #logic(optimal approach)
 for i in range(n):
-    for j in range(i+1,n+1):
+    for j in range(i+1,n):
         if target - list1[j] == list1[i]:
             print([i,j])
     
