ca1d47da7c8ca5e246769daf7b788b4c64a4ca54
ayushgarg9 <ayushgarg9707@gmail.com>
2025-07-28 00:42:56 +0530

    TWO SUM

---
+++ twosum.py
@@ -0,0 +1,10 @@
+list1 = [9,5,7,1,4,3]
+target = 13
+n = len(list1)
+#logic(optimal approach)
+for i in range(n):
+    for j in range(i+1,n+1):
+        if target - list1[j] == list1[i]:
+            print([i,j])
+    
+    
